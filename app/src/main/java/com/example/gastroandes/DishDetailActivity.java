a507ca5f2e2a8431383b964c3daf04a74f1afc26
HsanAnto <santiag0antolin3z2003@gmail.com>
2024-09-21 15:52:10 -0500

    Vista DONE

---
+++ app/src/main/java/com/example/gastroandes/DishDetailActivity.java
@@ -0,0 +1,6 @@
+package com.example.gastroandes;
+
+import android.app.Activity;
+
+public class DishDetailActivity extends Activity {
+}
--- app/src/main/java/com/example/gastroandes/MainActivity.kt
+++ app/src/main/java/com/example/gastroandes/MainActivity.kt
@@ -1,17 +1,16 @@
-package com.example.gastroandes.view
+package com.example.gastroandes
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.gastroandes.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login_v1)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
