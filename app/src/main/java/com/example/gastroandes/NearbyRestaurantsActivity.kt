d90814aeaa996e4cf6a4cfc42941bd9e3e57010a
Juan Orduz <juanorpinilla@gmail.com>
2024-09-22 21:04:08 -0500

    Vista 8 nearby restaurants

---
+++ app/src/main/java/com/example/gastroandes/NearbyRestaurantsActivity.kt
@@ -0,0 +1,42 @@
+package com.example.gastroandes
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class NearbyRestaurantsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var map: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.nearby_restaurants_v8)
+        createFragment()
+    }
+
+    private fun createFragment() {
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        createMarker()
+    }
+
+    private fun createMarker() {
+        val coordinate = LatLng(4.603043, -74.065774)
+        val marker = MarkerOptions().position(coordinate).title("GastroAndes")
+        map.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(coordinate, 17f),
+            3000,
+            null
+        )
+    }
+}
